16dc5df45f060bdb68fe1c0fee215801b276300c
Subir Mansukhani <subir.m@gmail.com>
2024-02-27 18:33:07 -0800

    Update 2_About_Us.py

--- pages/2_About_Us.py
+++ pages/2_About_Us.py
@@ -21,7 +21,7 @@ had a special role to play in creating World Helper. Megan is head
 of organization; Vidya leads on the technology side; and Luna
 brings our artwork to life.\n\n""")
 
-st.write("\n\n")
+st.write("\n\n\n\n")
 
 for member in team_members:
     col1, col2 = st.columns([1, 2])
