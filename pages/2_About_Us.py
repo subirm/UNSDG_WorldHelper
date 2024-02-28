6d7dbb9d89af5bb98c4821f9d6356f33e8c6cc98
Subir Mansukhani <subir.m@gmail.com>
2024-02-27 18:09:21 -0800

    Update 2_About_Us.py

--- pages/2_About_Us.py
+++ pages/2_About_Us.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 # Team Members Information
 team_members = [
-    {"name": "John Doe", "role": "Data Scientist", "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "profile_pic": "../assets/megan.jpg"},
-    {"name": "Jane Smith", "role": "Software Engineer", "bio": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "profile_pic": "../assets/luna.jpg"},
-    {"name": "Alice Johnson", "role": "UX Designer", "bio": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.", "profile_pic": "../assets/vidya.jpg"}
+    {"name": "John Doe", "role": "Data Scientist", "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "profile_pic": "./assets/megan.jpg"},
+    {"name": "Jane Smith", "role": "Software Engineer", "bio": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "profile_pic": "./assets/luna.jpg"},
+    {"name": "Alice Johnson", "role": "UX Designer", "bio": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.", "profile_pic": "./assets/vidya.jpg"}
 ]
 
 # Display Team Members
