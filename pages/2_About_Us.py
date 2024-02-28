6208985a9e117e8c9aec9de387899c2bf4fa023d
Subir Mansukhani <subir.m@gmail.com>
2024-02-27 19:01:00 -0800

    Update 2_About_Us.py

--- pages/2_About_Us.py
+++ pages/2_About_Us.py
@@ -1,32 +1,39 @@
 import streamlit as st
 
-# Function to display team members
-def display_team_members():
-    # Team Members Information
-    team_members = [
-        {"name": "Megan", "role": "Team Leader", "bio": "Megan bio", "profile_pic": "./assets/megan.jpg"},
-        {"name": "Luna", "role": "Design and Artwork", "bio": "Luna bio", "profile_pic": "./assets/luna.jpg"},
-        {"name": "Vidya", "role": "Technology Lead", "bio": "Vidya bio", "profile_pic": "./assets/vidya.jpg"}
-    ]
+with st.sidebar:
+    st.image("./assets/pippy.jpg", width=200)
 
-    # Display Team Members
-    for member in team_members:
-        col1, col2 = st.columns([1, 4])
+# Team Members Information
+team_members = [
+    {"name": "Megan", "role": "Team Leader", "bio": "Megan bio", "profile_pic": "./assets/megan.jpg"},
+    {"name": "Luna", "role": "Design and Artwork", "bio": "Luna bio", "profile_pic": "./assets/luna.jpg"},
+    {"name": "Vidya", "role": "Technology Lead", "bio": "Vidya bio", "profile_pic": "./assets/vidya.jpg"}
+]
 
-        with col1:
-            st.image(member["profile_pic"], width=200)
+# Display Team Members
+st.title("Team Cherry Blossoms")
+st.empty()
 
-        with col2:
-            st.subheader(member["name"])
-            st.write(f"**Role:** {member['role']}")
-            st.write(member["bio"])
+st.write("""World Helper is an app that helps communities make the world a
+better place. This app helps you learn about the UN Sustainable
+Development Goals. It also helps you plan events that support the
+UN Sustainable Development Goals. \n 
+World Helper was created by Megan, Vidya, and Luna. We are
+three friends who attend Osler Elementary School in Vancouver,
+Canada. This is our second Technovation project. We each have
+had a special role to play in creating World Helper. Megan is the team leader; Vidya leads on the technology side, and Luna
+brings our artwork to life.\n\n""")
 
-# Main Page
-def main_page():
-    st.title("Team Cherry Blossoms")
-    # Load the content for this specific page
-    display_team_members()
+st.write("\n\n\n\n")
 
-# Display the main page
-# st.rerun()
-main_page()
+for member in team_members:
+    col1, col2 = st.columns([1, 2])
+    
+    with col1:
+        st.image(member["profile_pic"], width=200)
+    
+    with col2:
+        st.write("\n")
+        st.markdown(f"<h4>{member['name']}</h4>", unsafe_allow_html=True)  # Adjust the header size here
+        st.write(f"**Role:** {member['role']}")
+        st.write(member["bio"])
