cb94c34309f45c05b6dd392056136ff90e97c145
Subir Mansukhani <subir.m@gmail.com>
2024-03-16 10:20:41 -0700

    Update 1_Plan_an_Event.py

--- pages/1_Plan_an_Event.py
+++ pages/1_Plan_an_Event.py
@@ -120,7 +120,8 @@ st.set_page_config(page_title="Plan an event")
 st.markdown("# Plan an event \n")
 st.markdown("""
 **Plan an Event and find out which UN Sustainable Development Goals your event supports!**  
-**We are so excited that you want to plan an event that supports the UNSDG.\n**  
+**We are so excited that you want to plan an event that supports the UNSDG.**
+
 **Fill in the details below and click “Submit” to find out which UNSDG your event supports!**  
 **You might be surprised and learn something new!**
 """)
